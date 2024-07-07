42ed0ee87197dc1ac73d74dcd713f744f6c273e9
mlfearon <mlfearon@umich.edu>
2024-07-07 16:18:36 -0400

    remove old data files not needed for final analyses

--- matlab-little-r/calc_r_Fx.m
+++
@@ -1,9 +0,0 @@
-function sumall = calc_r_Fx(r, Fx, lx, x)
-
-x=x(1:end-1);
-lx=lx(1:end-1);
-erx=exp(-r*x);
-
-sumall = sum(Fx.*lx.*erx)-1;
-sumall = abs(sumall);
-
--- matlab-little-r/calc_r_mx.m
+++
@@ -1,7 +0,0 @@
-function sumall = calc_r_mx(r, mx, lx, x)
-
-erx=exp(-r*x);
-
-sumall = sum(lx.*mx.*erx)-1;
-sumall = abs(sumall);
-
